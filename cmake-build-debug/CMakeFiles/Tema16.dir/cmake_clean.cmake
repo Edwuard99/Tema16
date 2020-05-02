d530bc5a46968d42e40ccae37f988dc8eae77bba
Vlad Eduard Diaconescu <edidiaconescu@gmail.com>
2020-05-02 21:57:58 +0300

    Project created

---
+++ cmake-build-debug/CMakeFiles/Tema16.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tema16.dir/main.cpp.obj"
+  "Tema16.exe"
+  "Tema16.exe.manifest"
+  "Tema16.pdb"
+  "libTema16.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tema16.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
